e15b57cf434f0f5c89915b26fa8dc7dc77177f77
yavidor <yonatan@avidor.org>
2024-04-21 20:23:22 +0300

    Fixed self reference in util package

--- util/ipc_client.go
+++ util/ipc_client.go
@@ -6,7 +6,7 @@ import (
 	"net"
 )
 
-func MakeIpcClient(logger *util.Logger) (net.Conn, IDisposable) {
+func MakeIpcClient(logger *Logger) (net.Conn, IDisposable) {
 	conn, err := net.Dial("unix", COGO_CONN_UINX)
 	if err != nil {
 		logger.Error("Failed to connect to background process", "err", err, "addr", COGO_CONN_UINX)
